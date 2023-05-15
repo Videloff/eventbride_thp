304251a74ffb871ad1ce20f7fb09b30183adda4d
vincent <kratos_angel_59@hotmail.fr>
2023-05-15 15:38:24 +0200

    add user mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+  after_create :welcome_send
   
   has_many :events, through: :attendances
   has_many :attendances
 
   has_many :admins, foreign_key:'admin_id', class_name: 'Event'
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
   
 end
