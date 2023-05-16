17c64075e1c10a03e072405fd438309c852e5a27
vincent <kratos_angel_59@hotmail.fr>
2023-05-16 13:51:38 +0200

    edit production.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,5 @@ end
 
 # Créer 5 events
 3.times do
-  Event.create(
-    title: Faker::JapaneseMedia::OnePiece.quote,
-    start_date: Faker::Date.forward(days: 10),
-    duration: [5,10,15,20,25,30].sample,
-    description: Faker::Lorem.sentence(word_count: 15),
-    price: Faker::Number.between(from: 1, to: 1000),
-    location: Faker::JapaneseMedia::OnePiece.location,
-    admin_id: User.all.sample.id)
+  Event.create(title: Faker::JapaneseMedia::OnePiece.quote, start_date: Faker::Date.forward(days: 10), duration: [5,10,15,20,25,30].sample, description: Faker::Lorem.sentence(word_count: 15), price: Faker::Number.between(from: 1, to: 1000), location: Faker::JapaneseMedia::OnePiece.location, admin_id: User.all.sample.id)
 end
