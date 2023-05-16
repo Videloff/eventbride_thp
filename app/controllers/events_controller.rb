d72efd766fa9a70dd67355c0961ab38d83d26df3
vincent <kratos_angel_59@hotmail.fr>
2023-05-16 23:17:53 +0200

    add all event in index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
 
   def index
-
+    @events = Event.all
   end
 
   def new
@@ -9,15 +9,20 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location])
-    @event.admin_id = current_user.id
+    if session[:user_id]
+      @event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location])
+      @event.admin_id = current_user.id
 
-    if @event.save
-      # redirect_to event_path(@event.id)
-      redirect_to root_path
-      flash[:success]= "Ton évenement a bien été créé !"
+      if @event.save
+        # redirect_to event_path(@event.id)
+        redirect_to root_path
+        flash[:success]= "Ton évenement a bien été créé !"
+      else
+        render new_event_path
+      end
     else
-      render new_event_path
+      flash[:alert] = "Vous devez être connecté pour créer un potin"
+      redirect_to root_path
     end
   end
 
