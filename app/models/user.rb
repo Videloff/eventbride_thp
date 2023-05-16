9cd6d08484354f08017339de638a5d68181c2906
vincent <kratos_angel_59@hotmail.fr>
2023-05-16 12:07:02 +0200

    devise implementation + bootstrap navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   after_create :welcome_send
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   
   has_many :attendances
   has_many :events, through: :attendances
