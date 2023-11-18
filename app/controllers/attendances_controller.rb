d89b50f4501c830cd9f70d94e7c5e6c0a313d8a7
vincent <kratos_angel_59@hotmail.fr>
2023-11-18 02:04:57 +0100

    delete puts on codes

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,10 +4,6 @@ class AttendancesController < ApplicationController
     @attend = Attendance.new
     @event = Event.find(params[:event_id])
     @registered = false
-    puts '#' * 120
-    puts params
-    puts Attendance.find_by(user_id: current_user.id, event_id: params[:event_id])
-    puts '#' * 120
     @check = @event.attendances.all.find_by(user_id: current_user.id)
     if @check
       @registered = true
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,9 +21,6 @@ class EventsController < ApplicationController
 
   def create
     if current_user
-      puts '$' * 100
-      puts params[:event_picture]
-      puts '$' * 100
       @event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location])
       @event.admin_id = current_user.id
       @event.event_picture = params[:event_picture]
